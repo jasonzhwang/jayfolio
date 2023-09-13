4f1e4bbd1345e16a1dde29f66eb1fb76d6b78c23
Jason Wang <0212jason@gmail.com>
2023-09-13 15:27:29 +1000

    fix: add secret

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,45 +1,12 @@
-import NextAuth from "next-auth";
-import Providers from "next-auth/providers";
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
   providers: [
-    Providers.Google({
+    GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-    // Add other authentication providers if needed
   ],
-
-  callbacks: {
-    async signIn(user, account, profile) {
-      return true; // Allow sign-in to proceed
-    },
-    async redirect(url, baseUrl) {
-      return baseUrl; // Redirect to the home page after sign-in
-    },
-    async session(session, user) {
-      session.user = user;
-      return session;
-    },
-    async jwt(token, user) {
-      if (user) {
-        token.id = user.id; // You can add more user data to the token if needed
-      }
-      return token;
-    },
-  },
-
-  pages: {
-    signIn: "/auth/signin", // Customize the sign-in page URL
-  },
-
-  // Add other NextAuth.js configuration options as needed
-
-  // Add custom error handling
-  callbacks: {
-    async onError(error, _, __) {
-      console.error("NextAuth Error:", error.message);
-      throw error;
-    },
-  },
+  secret: process.env.JWT_SECRET,
 });
